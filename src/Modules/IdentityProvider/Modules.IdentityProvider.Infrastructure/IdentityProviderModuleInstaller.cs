b3e78e888d5f06c54613b49ff9c1374d63ee8bb1
Iuri Mignoni <iurimig@gmail.com>
2025-07-12 10:10:42 -0300

    [feat]: add configuration for the new version of MediatR

--- src/Modules/IdentityProvider/Modules.IdentityProvider.Infrastructure/IdentityProviderModuleInstaller.cs
+++ src/Modules/IdentityProvider/Modules.IdentityProvider.Infrastructure/IdentityProviderModuleInstaller.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Modules.IdentityProvider.Application.Interfaces;
 using Modules.IdentityProvider.Domain.Users;
@@ -14,20 +13,22 @@ using Modules.IdentityProvider.Endpoints.PublicAPI;
 using BuildingBlocks.Persistence.Options;
 using Microsoft.Extensions.Options;
 using System;
+using Microsoft.Extensions.Configuration;
+using BuildingBlocks.Utilities;
 
 
 namespace Modules.IdentityProvider.Infrastructure;
 
-public static class UserAccessModuleInstaller
+public static class IdentityProviderModuleInstaller
 {
-    public static IServiceCollection AddUserModule(this IServiceCollection services)
+    public static IServiceCollection AddUserModule(this IServiceCollection services, IConfiguration configuration)
     {
         services
             .AddScoped<IJwtProvider, JwtProvider>()
             .AddScoped<ICreateUserService, CreateUserService>();
 
         AddPersistence(services);
-        AddApplication(services);
+        AddApplication(services, configuration);
         AddPublicAPI(services);
 
         return services;
@@ -35,7 +36,7 @@ public static class UserAccessModuleInstaller
 
     private static void AddPersistence(IServiceCollection services)
     {
-        services.AddDbContext<UsersDbContext>((serviceProvider, options) =>
+        services.AddDbContext<IdentityDbContext>((serviceProvider, options) =>
         {
             var connectionString = serviceProvider.GetService<IOptions<ConnectionStringOptions>>()!.Value;
 
@@ -51,15 +52,21 @@ public static class UserAccessModuleInstaller
             options.Password.RequireDigit = false;
             options.Password.RequiredLength = 8;
         })
-        .AddEntityFrameworkStores<UsersDbContext>()
+        .AddEntityFrameworkStores<IdentityDbContext>()
         .AddDefaultTokenProviders()
         .AddRoles<IdentityRole<Guid>>();
     }
 
-    private static void AddApplication(IServiceCollection services)
+    private static void AddApplication(IServiceCollection services, IConfiguration configuration)
     {
         services
-            .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Application.AssemblyReference.Assembly));
+            .AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Application.AssemblyReference.Assembly);
+
+                cfg.LicenseKey = SettingsUtils
+                    .GetEnvironmentValueOrDefault("MEDIATR_KEY", configuration.GetValue<string>("MediatRKey") ?? string.Empty);
+            });
     }
 
     private static void AddPublicAPI(IServiceCollection services)
