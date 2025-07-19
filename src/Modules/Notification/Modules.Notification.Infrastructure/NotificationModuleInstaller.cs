6fc4a87296a87f56c44374f92975d2316fe372f7
Iuri Mignoni <iurimig@gmail.com>
2025-07-19 15:19:55 -0300

    [feat]: add email sender

--- src/Modules/Notification/Modules.Notification.Infrastructure/NotificationModuleInstaller.cs
+++ src/Modules/Notification/Modules.Notification.Infrastructure/NotificationModuleInstaller.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
+using Modules.Notification.Application.Abstractions;
 using Modules.Notification.Infrastructure.Consumers;
+using Modules.Notification.Infrastructure.Emails;
 
 namespace Modules.Notification.Infrastructure;
 
@@ -7,6 +9,8 @@ public static class NotificationModuleInstaller
 {
     public static IServiceCollection AddNotificationModule(this IServiceCollection services)
     {
+        services.ConfigureOptions<EmailSenderOptionsSetup>();
+        services.AddTransient<IEmailSender, EmailSender>();
         services.AddIntegrationEventHandlers();
         services.AddConsumers();
 
